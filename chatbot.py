4aae7c6025c4545d3b2a5775eb6850ec2e6893bc
Cady Baltz <cady@Cadys-MacBook-Pro.local>
2023-04-03 23:36:21 -0500

    Added basic Python program to chat with Google DialogFlow agent

---
+++ chatbot.py
@@ -0,0 +1,34 @@
+from google.cloud import dialogflow_v2beta1 as dialogflow
+from google.protobuf.json_format import MessageToDict
+
+if __name__ == '__main__':
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path('s4395-travel-agent-bapg', 'current-user-id')
+
+    user_input = 'Hello'
+    while user_input != 'exit':
+        text_input = dialogflow.types.TextInput(text=user_input, language_code='en-US')
+        query_input = dialogflow.types.QueryInput(text=text_input)
+        response = session_client.detect_intent(session=session,query_input=query_input)
+
+        # convert response to a dictionary for parsing
+        response_dict = MessageToDict(response.query_result._pb)
+
+        # collect information about the user
+        parameters_dict = response_dict['parameters']
+        if 'person' in parameters_dict:
+            user_name = parameters_dict['person']['name']
+            print("LOG - Detected new user: " + user_name)
+
+        if 'geo-country' in parameters_dict:
+            country = parameters_dict['geo-country']
+            print("LOG - Detected new country: " + country)
+
+        # extract what information the user would like to know
+        if 'intent' in response_dict:
+            intent_name = response_dict['intent']['displayName']
+            print("LOG - Detected new user intent: " + intent_name)
+
+        endConversation = str(response.query_result.intent.display_name)
+        print(response.query_result.fulfillment_text)
+        user_input = input()
